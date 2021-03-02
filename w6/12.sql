5731af7a12c99ef8c34b54f8d2115f5d75a99e8e
timurnugaev <decter49pro@gmail.com>
2021-03-02 17:57:22 +0300

    w6

--- w6/12.sql
+++ w6/12.sql
@@ -1,4 +1,12 @@
 -- Task #1.2
-SELECT * FROM city, store, address
-where store.address_id = address.address_id
-	and address.address_id = city.city_id
+SELECT store.store_id, SUM(payment.amount) FROM store
+
+INNER JOIN staff ON store.store_id = staff.store_id
+INNER JOIN payment ON staff.staff_id = payment.staff_id
+
+WHERE (TO_CHAR(payment.payment_date, 'YYYY'),
+        TO_CHAR(payment.payment_date, 'MM'))
+= (SELECT TO_CHAR(payment_date, 'YYYY') AS y, TO_CHAR(payment_date, 'MM') AS m FROM payment
+  ORDER BY y DESC, m DESC LIMIT 1)
+
+GROUP BY store.store_id;
